6b2737fd6786312325ea4b9accf116ab6b80f63d
wollowizard <alfredo.scaccialepre@gmail.com>
2012-11-17 15:46:08 +0100

    Revert "database seems ok"
    This reverts commit 21f617886437ce64c576346bab31c6852ff86170.

--- id2212-fish/src/fish/database/DataBaseManager.java
+++ id2212-fish/src/fish/database/DataBaseManager.java
@@ -10,6 +10,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
